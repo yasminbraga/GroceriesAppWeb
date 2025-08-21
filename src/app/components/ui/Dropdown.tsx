6381a5bc597bc4692ce73e6ce896e636a39d818a
yasminbraga <yasminbragateixeira@gmail.com>
2025-08-21 17:12:27 -0300

    style: adiciona dropdown em listas

---
+++ src/app/components/ui/Dropdown.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+import Badge from "../Badge";
+
+const Dropdown = ({ lists }: { lists: ListType[] }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  return (
+    <div className="relative">
+      <button
+        className="flex gap-2 items-center justify-between p-2 border-1 border-gray-300 rounded-xl"
+        onClick={handleToggle}
+      >
+        <span>Listas</span>
+        {isOpen ? (
+          <span className="material-symbols-outlined">keyboard_arrow_up</span>
+        ) : (
+          <span className="material-symbols-outlined">keyboard_arrow_down</span>
+        )}
+      </button>
+
+      {isOpen && (
+        <div className="absolute left-1/2 top-10 bg-white shadow-lg p-4 rounded-lg min-w-[300px]">
+          {lists.map((list) => (
+            <Link
+              key={list.id}
+              href={`http://localhost:3000/lists/${list.id}`}
+              className="flex justify-between items-center"
+            >
+              <div className="flex gap-2 flex-col mr-6">
+                <p>{list.title}</p>
+
+                {list.recipe ? <Badge /> : null}
+              </div>
+              <span className="material-symbols-outlined">chevron_right</span>
+            </Link>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Dropdown;
--- src/app/lists/layout.tsx
+++ src/app/lists/layout.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Badge from "../components/Badge";
+import Dropdown from "../components/ui/Dropdown";
 export default async function ListLayout({
   children,
 }: {
@@ -8,10 +9,10 @@ export default async function ListLayout({
   const data = await fetch("http://localhost:8080/lists");
   const lists: ListType[] = await data.json();
   return (
-    <div className="flex h-full">
-      <div className="border-r-1 border-r-gray-300 w-[300px]">
+    <div className="flex flex-col md:flex-row h-full">
+      <div className="border-r-1 border-r-gray-300 w-[300px] hidden md:block">
         <header className="flex items-center justify-between border-b-1 border-b-gray-300 p-4">
-          <h2 className="text-lg font-semibold ">Listas</h2>
+          <h2 className="text-lg font-semibold">Listas</h2>
           <Link
             href={"/lists/create"}
             className="flex items-center gap-2 bg-amber-500 rounded text-white py-1 px-2 font-medium text-sm cursor-pointer"
@@ -38,6 +39,23 @@ export default async function ListLayout({
           ))}
         </div>
       </div>
+
+      <div className="border-b-1 border-b-gray-300 p-4 md:hidden">
+        <h2 className="text-lg font-semibold mb-4">Listas</h2>
+        <div className="flex justify-between items-center">
+          <Dropdown lists={lists} />
+          <Link
+            href={"/lists/create"}
+            className="flex items-center gap-2 bg-amber-500 rounded text-white py-1 px-2 font-medium text-sm cursor-pointer"
+          >
+            <span className="material-symbols-outlined !text-[18px] p-2 rounded-xl">
+              add
+            </span>
+            <p>Nova lista</p>
+          </Link>
+        </div>
+      </div>
+
       <article className="p-6 w-full bg-gray-50">{children}</article>
     </div>
   );
