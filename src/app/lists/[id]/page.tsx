1871806686db440feedd29313221f63e90913885
yasminbraga <yasminbragateixeira@gmail.com>
2025-09-02 16:24:49 -0300

    style: ajusta pagina de show listas

--- src/app/lists/[id]/page.tsx
+++ src/app/lists/[id]/page.tsx
@@ -17,14 +17,22 @@ export default async function List({
 
   return (
     <>
-      <h3 className="text-xl font-semibold mb-2">{list.title}</h3>
+      <div className="mb-4">
+        <h3 className="text-3xl font-bold mb-3">{list.title}</h3>
+        <p className="text-sm text-gray-400">Criado em: 01/09/2025</p>
+      </div>
 
       <div>
         {list.products.map((product) => (
-          <div key={product.id} className="flex items-center gap-1">
+          <div
+            key={product.id}
+            className="flex items-center gap-3 mb-3 text-gray-700"
+          >
             <CheckboxInput id={product.id} checked={product.checked} />
-            <p>{product.quantity}</p>
-            <p>{product.name}</p>
+            <div className="flex items-center gap-1">
+              <p>{product.quantity}</p>
+              <p>{product.name}</p>
+            </div>
             <DeleteProduct id={product.id} />
           </div>
         ))}
